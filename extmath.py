8114c21320adcba773596c674707192a74008a1b
MrBmin <benjaminvanwingen158@gmail.com>
2026-03-22 14:37:05 +0000

    Testing extmath

--- extmath.py
+++ extmath.py
@@ -2,10 +2,10 @@ import math
 try:
     #square root
     if cmd1 == "-sqrt":
-        print(math.sqrt(cmd2))
+        print(math.sqrt(int(cmd2)))
      #exponentiation (extmath -exp a b = a^b)
     if cmd1 == "-exp":
-        print(math.exp(cmd1))
+        print(math.exp(int(cmd1)))
     if cmd1 == "-?":
         print("extmath -sqrt a - Returns the square root of a\nextmath -exp a - Returns Euler's Number (~2.718) raised to the power of a\nextmath -ctn a - Counts to a (e.g. extmath -ctn 5 outputs 1, 2, 3, 4, 5)")
 
@@ -13,7 +13,7 @@ try:
     if cmd1 == "ctn":
         for i in range(int(cmd2)):
             print(f"{i}", end="")
-        if not i == int(cmd2):
+            if not i == int(cmd2):
                 print(", ", end="")
 except:
     print("One or more arguments were not numbers!")
