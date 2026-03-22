585986cd2e2c64f448951dc96857ccc0f406be3b
MrBmin <benjaminvanwingen158@gmail.com>
2026-03-22 09:58:18 +0000

    Testing the extmath optional feature

--- extmath.py
+++ extmath.py
@@ -1,24 +1,26 @@
 import math
 
-try:
-    #square root
-    if cmd1 == "-sqrt":
-        print(math.sqrt(cmd2))
+#this is just to make one extra indent cus for some reason the installer never seems to install extmath with the correct indent
+if True:
+    try:
+        #square root
+        if cmd1 == "-sqrt":
+            print(math.sqrt(cmd2))
 
-    #exponentiation (extmath -exp a b = a^b)
-    if cmd1 == "-exp":
-        print(math.exp(cmd2, cmd3))
+        #exponentiation (extmath -exp a b = a^b)
+        if cmd1 == "-exp":
+            print(math.exp(cmd1))
 
-    if cmd1 == "-?":
-        print("extmath -sqrt a - Returns the square root of a\nextmath -exp a b - Returns a raised to the power of b\nextmath -ctn a - Counts to a (e.g. extmath -ctn 5 outputs 1, 2, 3, 4, 5)")
+        if cmd1 == "-?":
+        print("extmath -sqrt a - Returns the square root of a\nextmath -exp a - Returns Euler's Number (~2.718) raised to the power of a\nextmath -ctn a - Counts to a (e.g. extmath -ctn 5 outputs 1, 2, 3, 4, 5)")
 
-    #counts to n (extmath -ctn 10 outputs 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-    if cmd1 == "ctn":
-        for i in range(cmd2):
-            print(f"{i}", end="")
-            if not i == cmd2:
-                print(", ", end="")
-    
+        #counts to n (extmath -ctn 10 outputs 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+        if cmd1 == "ctn":
+            for i in range(int(cmd2)):
+                print(f"{i}", end="")
+            if not i == int(cmd2):
+                    print(", ", end="")
 
-except:
-    print("One or more arguments were not numbers!")
+
+    except:
+        print("One or more arguments were not numbers!")
